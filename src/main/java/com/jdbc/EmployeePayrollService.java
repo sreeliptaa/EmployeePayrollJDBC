ce9a497a951d00ddcbff5aeb8bb1e594e608b9b8
sreeliptaa <sreelipta65@gmail.com>
2021-10-09 07:11:55 +0530

    Added: making payroll database as singleton object

--- src/main/java/com/jdbc/EmployeePayrollService.java
+++ src/main/java/com/jdbc/EmployeePayrollService.java
@@ -11,18 +11,18 @@ public class EmployeePayrollService {
         return printSet(queries);
     }
 
-    public int updateData(String name, double value) throws SQLException {
+    public int updateData(String name,double value) throws SQLException {
         employeePayrollJdbc = EmployeePayrollJdbc.getInstance();
         Connection connection = employeePayrollJdbc.dbConnect();
         Statement statement = connection.createStatement();
-        String query = String.format("UPDATE employee_payroll SET salary = %.2f WHERE name = '%s';", value, name);
+        String query = String.format("update payroll set basic_pay = '%.2f' where emp_id IN (select emp_id from employee where name = '%s');",value,name);
         return statement.executeUpdate(query);
     }
 
     public int updatePreparedData(String name,double value) throws SQLException {
         employeePayrollJdbc = EmployeePayrollJdbc.getInstance();
         Connection connection = employeePayrollJdbc.dbConnect();
-        String query = String.format("UPDATE employee_payroll SET Basic_pay = %.2f WHERE name = '%s';", value, name);
+        String query = String.format("update payroll set basic_pay = '%.2f' where emp_id IN (select emp_id from employee where name = '%s');",value,name);
         PreparedStatement statement =  connection.prepareStatement(query);
         return statement.executeUpdate();
     }
@@ -34,7 +34,6 @@ public class EmployeePayrollService {
 
         return statement.executeQuery(query);
     }
-
     public int printSet(ResultSet queries) throws SQLException {
         int i = 0;
         while (queries.next()) {
@@ -46,6 +45,12 @@ public class EmployeePayrollService {
             System.out.println(id + " " + name + " " + salary + " " + start + " ");
         }
         return i;
+    }
+
+    public int retrieveDate() throws SQLException {
+        String query = String.format("Select * from employee_payroll where start between '%s' and '%s');", Date.parse("2018-01-01"),Date.parse("2021-12-30"));
+        ResultSet queries = getQuerries(query);
+        return printSet(queries);
     }
 }
 
